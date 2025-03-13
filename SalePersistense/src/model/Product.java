e862abdbf6f89399e50be5146ce9a728c43e1ffe
Magnusslothh1 <10632116@ucn.dk>
2025-03-13 09:11:04 +0100

    SaleOrder getters and setters

--- SalePersistense/src/model/Product.java
+++ SalePersistense/src/model/Product.java
@@ -2,4 +2,62 @@ package model;
 
 public class Product {
 
+	private double purchasePrice; 
+	private double rentPrice;
+	private String countryOfOrigin;
+	private int minStock;
+	private int amount;
+	private int reservedAmount;
+	private int productNo;	
+	
+	private String name; 
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPurchasePrice() {
+		return purchasePrice;
+	}
+	public void setPurchasePrice(double purchasePrice) {
+		this.purchasePrice = purchasePrice;
+	}
+	public double getRentPrice() {
+		return rentPrice;
+	}
+	public void setRentPrice(double rentPrice) {
+		this.rentPrice = rentPrice;
+	}
+	public String getCountryOfOrigin() {
+		return countryOfOrigin;
+	}
+	public void setCountryOfOrigin(String countryOfOrigin) {
+		this.countryOfOrigin = countryOfOrigin;
+	}
+	public int getMinStock() {
+		return minStock;
+	}
+	public void setMinStock(int minStock) {
+		this.minStock = minStock;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public int getReservedAmount() {
+		return reservedAmount;
+	}
+	public void setReservedAmount(int reservedAmount) {
+		this.reservedAmount = reservedAmount;
+	}
+	public int getProductNo() {
+		return productNo;
+	}
+	public void setProductNo(int productNo) {
+		this.productNo = productNo;
+	}
+	
 }
--- SalePersistense/src/model/SaleOrder.java
+++ SalePersistense/src/model/SaleOrder.java
@@ -1,5 +1,51 @@
 package model;
 
+import java.time.LocalDate;
+
 public class SaleOrder {
 
+	private String deliveryStatus;
+	private String deliveryDate;
+	private int saleOrderNo;
+	private Customer customer;
+	private OrderLine orderline;
+
+	
+private LocalDate date;
+public LocalDate getDate() {
+	return date;
+}
+public void setDate(LocalDate date) {
+	this.date = date;
+}
+public String getDeliveryStatus() {
+	return deliveryStatus;
+}
+public void setDeliveryStatus(String deliveryStatus) {
+	this.deliveryStatus = deliveryStatus;
+}
+public String getDeliveryDate() {
+	return deliveryDate;
+}
+public void setDeliveryDate(String deliveryDate) {
+	this.deliveryDate = deliveryDate;
+}
+public int getSaleOrderNo() {
+	return saleOrderNo;
+}
+public void setSaleOrderNo(int saleOrderNo) {
+	this.saleOrderNo = saleOrderNo;
+}
+public Customer getCustomer() {
+	return customer;
+}
+public void setCustomer(Customer customer) {
+	this.customer = customer;
+}
+public OrderLine getOrderline() {
+	return orderline;
+}
+public void setOrderline(OrderLine orderline) {
+	this.orderline = orderline;
+}
 }
