072e4cb8222e4ffebd56db1f34f0fd7138efe580
Magnusslothh1 <10632116@ucn.dk>
2025-03-13 09:32:29 +0100

    Update club and Private

--- SalePersistense/src/model/Club.java
+++ SalePersistense/src/model/Club.java
@@ -1,5 +1,20 @@
 package model;
 
-public class Club {
+public class Club extends Customer{
 
+	private int clubNo;
+	
+	public Club (int clubNo, String name, String address, String zipCode, String city, String country, String email) {
+		super(name, address, zipCode, city, country, email);
+		this.clubNo = clubNo;
+	}
+		
+		public int getClubNo() {
+			return clubNo;
+		}
+
+		public void setClubNo(int clubNo) {
+			this.clubNo = clubNo;
+		
+	}
 }
